6ff170bb8dbd82201f761844898b73d86a4da11b
fartg <catfoodbeer@gmail.com>
2024-02-17 04:48:10 -0500

    initial

---
+++ src/app/text.js
@@ -0,0 +1,24 @@
+const text = {
+    about_me: {
+        name: "Zach M.",
+        age: "21",
+        employment: "Under Contract",
+        loves: "Cats"
+    },
+    socials: {
+        twitter: {
+            user: "@passion",
+            link: "x.com/passion"
+        }
+    },
+    projects: {
+        rbxcat: {
+            description: "A ROBLOX game engine based off of attributes instead of normal values.",
+            link: "https://github.com/fartg/rbxcat",
+            authors: "test",
+            date: "test"
+        }
+    }
+}
+
+export default text;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        console: ['Consolas', "sans-serif"],
+        powershell: ["Segoe UI Variable", "sans-serif"]
+      }
     },
   },
   plugins: [],
