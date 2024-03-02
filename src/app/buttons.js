364b8fc003c07f6cd80c5c1f1ce608b1d77bacba
fartg <catfoodbeer@gmail.com>
2024-03-02 11:18:04 -0500

    fix some things

--- src/app/buttons.js
+++ src/app/buttons.js
@@ -3,10 +3,10 @@ let buttons = {};
 
 buttons.createButtons = function() {
     return ( 
-        <div className = "flex text-xl gap-3">
-            <text className = "text-center pt-0.5 w-10 h-full hover:bg-[#3E3E3E]">─</text>
-            <text className = "text-center w-10  h-full hover:bg-[#3E3E3E]">□</text>
-            <text className = "text-center pt-0.5 w-10 h-full hover:bg-[#C42B1C]">✕</text>
+        <div className = "flex text-lg gap-3">
+            <text className = "text-center pt-0.5 w-8 h-8 hover:bg-[#3E3E3E]">─</text>
+            <text className = "text-center w-8  h-8 hover:bg-[#3E3E3E]">□</text>
+            <text className = "text-center pt-0.5 w-8 h-8 hover:bg-[#C42B1C]">✕</text>
         </div>
     )
 }
